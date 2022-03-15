fec44026d053f9d524c3eb23dce98ac4cc0176c7
carloskauan <carloskauan190@gmail.com>
2022-03-15 20:34:45 -0300

    Adicionando outro Exemplo !

---
+++ Printing graphic characters/CaracteresGraficos(2).cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	system("cls");
+	// Caracteres Gráficos !
+	printf("\xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBb");
+	printf("\n\xBA Hello !!! \xBA");
+	printf("\n\xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBc");
+	printf("\n\n");
+	
+	system("PAUSE");
+	return 0;
+}
