d99846a93e1ea76e2d8aecfce71d65ec787342f2
carloskauan <carloskauan190@gmail.com>
2022-03-15 20:20:04 -0300

    Caracteres Gráficos - C

---
+++ Printing graphic characters/CaracteresGraficos.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	// Imprimindo Caracteres gráficos.
+	printf("\n\t\xDC\xDC\xDB\xDB\xDB\xDB\xDC\xDC");
+	printf("\n\t\xDFO\xDF\xDF\xDF\xDFO\xDF...");
+	printf("\n");
+	printf("\n\t\xDC\xDC\xDB \xDB\xDB\xDB\xDB\xDB\xDB");
+	printf("\n\t\xDFO\xDF\xDF\xDF\xDF\xDFOO\xDF...\n\n");
+	system("PAUSE");
+	return 0;
+}
